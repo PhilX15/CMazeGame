f9a877f8acb1826ef837ed4a89b33687a66978aa
PhilX15 <filipkal55@gmail.com>
2024-07-13 15:12:53 +0200

    Added menu

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "maze.h"
 #include "utils.h"
@@ -15,9 +16,31 @@ int maze_height = 13;
 int level_coins = 5;
 int collected_coins = 0;
 
-enum game_state_t game_state = GAME;
+enum game_state_t game_state = MENU;
 struct maze_t *maze = NULL;
 struct player_t *player = NULL;
+
+int scr_width = 0;
+int scr_height = 0;
+
+const char *logo[6] = {
+    "___  ___                _____                      ",
+    "|  \\/  |               |  __ \\                     ",
+    "| .  . | __ _ _______  | |  \\/ __ _ _ __ ___   ___ ",
+    "| |\\/| |/ _` |_  / _ \\ | | __ / _` | '_ ` _ \\ / _ \\",
+    "| |  | | (_| |/ |  __/ | |_\\ | (_| | | | | | |  __/",
+    "\\_|  |_/\\__,_/___\\___|  \\____/\\__,_|_| |_| |_|\\___|"
+};
+
+const char *play_text[2] = {
+    "[ PLAY ]",
+    "  PLAY  "
+};
+
+const char *exit_text[2] = {
+    "[ EXIT ]",
+    "  EXIT  "
+};
 
 void init_level() {
     wclear(stdscr);
@@ -30,11 +53,6 @@ void init_level() {
         player = NULL;
     }
 
-    if (level > max_level) {
-        game_state = END;
-        return;
-    }
-
     collected_coins = 0;
     level++;
     maze_width += 2;
@@ -42,6 +60,11 @@ void init_level() {
 
     if (level % 2 == 0) {
         level_coins++;
+    }
+
+    if (level > max_level) {
+        game_state = END;
+        return;
     }
 
     maze = init_maze(maze_width, maze_height);
@@ -59,6 +82,7 @@ void init_level() {
 
 void print_hud() {
     mvprintw(1, 1, "Level: %d/%d\tCoins: %d/%d", level, max_level, collected_coins, level_coins);
+    mvprintw(scr_height - 2, scr_width / 2 - strlen("Press 'q' to exit") / 2, "Press 'q' to exit");
 }
 
 int main() {
@@ -74,34 +98,71 @@ int main() {
     
     srand(time(0));
 
-    init_level();
-
-    int scr_width = 0;
-    int scr_height = 0;
     getmaxyx(stdscr, scr_height, scr_width);
+
+    int option = 0;
     
-    while (game_state == GAME) {
+    while (game_state != END) {
         int last_width = scr_width;
         int last_height = scr_height;
         getmaxyx(stdscr, scr_height, scr_width);
         if (last_width != scr_width || last_height != scr_height) {
             wclear(stdscr);
         }
+        int key = -1;
+        switch (game_state) {
+            case MENU:
+                for (int i = 0; i < 6 && scr_width > strlen(*(logo + i)) && scr_height > 20; i++) {
+                    mvprintw(i + 2, scr_width / 2 - strlen(*(logo + i)) / 2, "%s", *(logo + i));
+                }
+                box(stdscr, 0, 0);
+                if (option == 0) {
+                    mvprintw(scr_height / 2 - 1, scr_width / 2 - strlen(*play_text) / 2, "%s", *play_text);
+                    mvprintw(scr_height / 2 + 1, scr_width / 2 - strlen(*exit_text) / 2, "%s",*(exit_text + 1));
+                } else {
+                    mvprintw(scr_height / 2 - 1, scr_width / 2 - strlen(*play_text) / 2, "%s", *(play_text + 1));
+                    mvprintw(scr_height / 2 + 1, scr_width / 2 - strlen(*exit_text) / 2, "%s", *exit_text);
+                }
 
-        print_hud();
-        print_maze(maze, scr_width, scr_height);
-        mvprintw(maze->top_left_y + player->location.y, maze->top_left_x + player->location.x, "%s", PLAYER_S);
-        box(stdscr, 0, 0);
+                key = wgetch(stdscr);
 
-        int key = wgetch(stdscr);
-        move_player(maze, player, key);
+                switch (key) {
+                    case KEY_DOWN:
+                        option = 1;
+                        break;
+                    case KEY_UP:
+                        option = 0;
+                        break;
+                    case '\n':
+                        if (option == 0) {
+                            game_state = GAME;
+                            init_level();
+                        } else {
+                            game_state = END;
+                        }
+                        break;
+                }
+                break;
+            case GAME:
+                print_hud();
+                print_maze(maze, scr_width, scr_height);
+                mvprintw(maze->top_left_y + player->location.y, maze->top_left_x + player->location.x, "%s", PLAYER_S);
+                box(stdscr, 0, 0);
 
-        if (*(*(maze->maze + player->location.y) + player->location.x) == COIN_I) {
-            collected_coins++;
-            *(*(maze->maze + player->location.y) + player->location.x) = SPACE_I;
-        }
-        if (collected_coins == level_coins) {
-            init_level();
+                key = wgetch(stdscr);
+                if (key == 'q') {
+                    game_state = END;
+                    break;
+                }
+                move_player(maze, player, key);
+
+                if (*(*(maze->maze + player->location.y) + player->location.x) == COIN_I) {
+                    collected_coins++;
+                    *(*(maze->maze + player->location.y) + player->location.x) = SPACE_I;
+                }
+                if (collected_coins == level_coins) {
+                    init_level();
+                }
         }
 
         refresh();
